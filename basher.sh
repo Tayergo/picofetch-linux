5e41c720e657470cd5c7c08f07b1cb95ac31b672
talwat <talrac1020@gmail.com>
2022-01-23 15:02:35 +0100

    Made it very gay.

--- basher.sh
+++ basher.sh
@@ -7,26 +7,53 @@ SPACE=$(df -h /dev/sdb3)
 CPU=$(grep -m 1 'model name' /proc/cpuinfo)
 GPU=$(glxinfo | grep "Device")
 RAM=$(free -h)
+Color_Off='\033[0m'       # Text Reset
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+BRed='\033[1;31m'         # Red
+BGreen='\033[1;32m'       # Green
+BYellow='\033[1;33m'      # Yellow
+BBlue='\033[1;34m'        # Blue
+BPurple='\033[1;35m'      # Purple
+BWhite='\033[1;37m'       # White
 
 clear
-echo "------LOGO--------"
+echo -e "$BWhite------LOGO--------$Color_Off"
 echo "$LOGO"
-echo "------OS VER------"
+
+echo -e "$BRed------OS VER------$Red"
 echo "$OS1"
 echo "$OS2"
-echo "------KERNEL------"
+echo
+
+echo -e "$BYellow------KERNEL------$Yellow"
 echo "$KERNEL"
-echo "------FREE SPACE------"
+echo
+
+echo -e "$BGreen------FREE SPACE------$Green"
 echo "$SPACE"
-echo "-----CPU MODEL------"
+echo
+
+echo -e "$BBlue-----CPU MODEL------$Blue"
 echo "$CPU"
-echo "------GPU MODEL-----"
+echo
+
+echo -e "$BPurple------GPU MODEL-----$Purple"
 echo "$GPU"
-echo "------RAM INFO-----"
+echo
+
+echo -e "$BRed------RAM INFO-----$Red"
 echo "$RAM"
-echo "-------SHELL----"
+echo
+
+echo -e "$BYellow-------SHELL----$Yellow"
 echo "$SHELL"
-echo "----------PACKAGES----"
+echo
+
+echo -e "$BGreen----------PACKAGES----$Green"
 # The next command needs to have the .txt cleared before running for accuracy.
 pacman -Qq --color never > packnum.txt
 wc -l packnum.txt
