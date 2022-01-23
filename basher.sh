cfabe4db1901886aa9eede9f6c4f14b3720599b4
talwat <talrac1020@gmail.com>
2022-01-23 14:43:38 +0100

    Cleaned up some of the code by implementing variables

--- basher.sh
+++ basher.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
+LOGO=$(tiv -h 25 -w 25 ~/picofetch/snak.png)
+OS1=$(grep '^VERSION' /etc/os-release)
+OS2=$(grep -E '^(VERSION|NAME)=' /etc/os-release)
+KERNEL=$(uname -r)
+SPACE=$(df -h /dev/sdb3)
+CPU=$(grep -m 1 'model name' /proc/cpuinfo)
+GPU=$(glxinfo | grep "Device")
+RAM=$(free -h)
 
 clear
 echo "------LOGO--------"
-tiv -h 25 -w 25 ~/picofetch/snak.png
+echo "$LOGO"
 echo "------OS VER------"
-grep '^VERSION' /etc/os-release
-egrep '^(VERSION|NAME)=' /etc/os-release
+echo "$OS1"
+echo "$OS2"
 echo "------KERNEL------"
-uname -r
+echo "$KERNEL"
 echo "------FREE SPACE------"
-df -h /dev/sdb3
+echo "$SPACE"
 echo "-----CPU MODEL------"
-grep -m 1 'model name' /proc/cpuinfo
+echo "$CPU"
 echo "------GPU MODEL-----"
-glxinfo | grep "Device"
+echo "$GPU"
 echo "------RAM INFO-----"
-free -h
+echo "$RAM"
 echo "-------SHELL----"
-echo $SHELL
+echo "$SHELL"
 echo "----------PACKAGES----"
 # The next command needs to have the .txt cleared before running for accuracy.
 pacman -Qq --color never > packnum.txt
