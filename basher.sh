3f692a974aa9162e391914d8ae36f97f8a82edbe
Xeon D. R <helpf446@gmail.com>
2022-02-15 06:01:40 -0600

    removed non-needed blank lines

--- basher.sh
+++ basher.sh
@@ -11,15 +11,11 @@ BYellow='\033[1;33m'      # Yellow
 BBlue='\033[1;34m'        # Blue
 BPurple='\033[1;35m'      # Purple
 BWhite='\033[1;37m'       # White
-
-
 # LOGO=$(snak.txt)
 OS=$(grep -m1 "NAME=" < /etc/os-release | cut -d '"' -f 2)
-=======
 # LOGO=$(viu -w 25 ./your_image_here)
 OS1=$(grep '^VERSION' /etc/os-release)
 OS2=$(grep -E '^(VERSION|NAME)=' /etc/os-release)
-
 KERNEL=$(uname -r)
 DEWM=$($DESKTOP_SESSION)
 FREE_SPACE=$(df -h / | awk '{print $3}' | grep "^[0-9]")
@@ -28,43 +24,33 @@ SPACE=$(echo ${FREE_SPACE}/${TOTAL_SPACE})
 CPU=$(grep -m 1 'model name' /proc/cpuinfo)
 GPU=$(glxinfo | grep "Device")
 RAM=$(egrep 'MemTotal|MemFree|MemAvailable' /proc/meminfo)
-
 clear
 # echo -e "$BWhite------------LOGO--------------$Color_Off"
 # echo -e "$LOGO"
-
 echo -e "$BRed------------OS VER------------$Red"
 echo "$OS"
 echo
-
 echo -e "$BYellow------------KERNEL------------$Yellow"
 echo "$KERNEL"
 echo
-
 echo -e "$BWhite-------------DE/WM-------------$White"
 echo "$DEWM"
 echo
-
 echo -e "$BGreen----------FREE SPACE----------$Green"
 echo "$SPACE"
 echo
-
 echo -e "$BBlue----------CPU MODEL-----------$Blue"
 echo "${CPU:13}"
 echo
-
 echo -e "$BPurple----------GPU MODEL-----------$Purple"
 echo "${GPU:12}"
 echo
-
 echo -e "$BRed-----------RAM INFO-----------$Red"
 echo "$RAM"
 echo
-
 echo -e "$BYellow------------SHELL-------------$Yellow"
 echo "$SHELL"
 echo
-
 echo -e "$BGreen-----------PACKAGES-----------$Green"
 #=======
 sudo pacman -Qq --color never > arch-pacman
