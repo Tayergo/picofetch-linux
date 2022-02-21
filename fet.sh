1448a304578dbab1c30f8f3f48b81d356f7b0580
JhonnyRice <93940240+JhonnyRice@users.noreply.github.com>
2022-02-21 20:20:10 +0100

    Better >:)

--- fet.sh
+++ fet.sh
@@ -11,26 +11,24 @@ BYellow='\033[1;33m'
 BBlue='\033[1;34m'
 BPurple='\033[1;35m'
 BWhite='\033[1;37m' # White
-OS=$(grep 'NAME=' /etc/os-release)
 KERNEL=$(uname -r)
-SPACE=$(df -h / | awk '{print $3}' | grep "^[0-9]" && df -h /home | awk '{print $2}' | grep "^[0-9]")
+WM=${WAYLAND_DISPLAY:-$(xprop -id "$(xprop -root _NET_SUPPORTING_WM_CHECK | cut -d' ' -f5)" _NET_WM_NAME | cut -d'"' -f2)}
 CPU=$(grep -m 1 'model name' /proc/cpuinfo)
 GPU=$(lspci | grep -Em2 'VGA')
-RAM=$(egrep 'MemTotal|MemAvailable' /proc/meminfo)
 echo -e "$BRed------------OS VER------------$Red"
-echo "$OS"
+grep -m1 -o '[^NAME=]' '/etc/os-release' | cut -d '"' -f 2 | tr -d '\012\015' && printf "\n"
 echo -e "$BYellow------------KERNEL------------$Yellow"
 echo "$KERNEL"
 echo -e "$BWhite-------------DE/WM-------------$White"
-echo "${XDG_CURRENT_DESKTOP#*:-"${DESKTOP_SESSION##*/}"}"
+echo ${WM}
 echo -e "$BGreen----------FREE SPACE----------$Green"
-echo "$SPACE"
+df -h / | awk '{print $3}' | grep "^[0-9]" && df -h /home | awk '{print $2}' | grep "^[0-9]"
 echo -e "$BBlue----------CPU MODEL-----------$Blue"
 echo "${CPU:13}"
 echo -e "$BPurple----------GPU MODEL-----------$Purple"
 echo "${GPU:12}"
 echo -e "$BRed-----------RAM INFO-----------$Red"
-echo "$RAM"
+egrep 'MemTotal|MemAvailable' /proc/meminfo
 echo -e "$BYellow------------SHELL-------------$Yellow"
 echo "${SHELL##*/}"
 echo -e "$BGreen-----------PACKAGES-----------$Green"
