844001a03482fd4a092d5878ac31813c3a00e9ad
Airei D. R <helpf446@gmail.com>
2022-08-22 20:38:00 -0500

    More debloating

--- fet.sh
+++ fet.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 WM=${WAYLAND_DISPLAY:-$(xprop -id "$(xprop -root _NET_SUPPORTING_WM_CHECK | cut -d' ' -f5)" _NET_WM_NAME | cut -d'"' -f2)}
-CPU=$(grep -m 1 'model name' /proc/cpuinfo)
-GPU=$(lspci | grep -Em2 'VGA' | cut -c 24-5000)
 echo -e "------------OS VER------------"
 grep -m1 -o '[^NAME=]' '/etc/os-release' | cut -d '"' -f 2 | tr -d '\012\015' && printf "\n"
 echo -e "------------KERNEL------------"
@@ -11,9 +9,9 @@ echo ${WM}
 echo -e "----------DISKS----------"
 df -h --output=source,size,used,avail | grep --color=never -e '^/' -e Filesystem
 echo -e "----------CPU MODEL-----------"
-echo "${CPU:13}"
+grep -m 1 'model name' /proc/cpuinfo
 echo -e "----------GPU MODEL-----------"
-echo "${GPU:12}"
+lspci | grep -Em2 'VGA' | cut -c 24-5000
 echo -e "-----------RAM INFO-----------"
 egrep 'MemTotal|MemAvailable' /proc/meminfo
 echo -e "------------SHELL-------------"
